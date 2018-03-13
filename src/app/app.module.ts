7fa1714c04ac0a115420cc49bd431825555fe70c
Lucas  Bertoldo <lucas.fber@gmail.com>
2018-03-13 09:32:22 -0300

    error by TestComponent fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavegadorComponent } from './navegador/navegador.component';
 import { PostComponent } from './post/post.component';
-import { TesteComponent } from './teste/teste.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavegadorComponent,
-    PostComponent,
-    TesteComponent
+    PostComponent
   ],
   imports: [
     BrowserModule
